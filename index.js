e2d77c4c3ed810107e4372f4cb8ad031a33e1bf1
Monty Anderson <monty.linux@gmail.com>
2015-07-14 20:32:13 +0100

    returns 0 if it's already started

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function yrs() {
     var start = new Date("July 27, 2015 10:00:00"),
         now   = new Date();
 
+    if(start.getTime() < now.getTime()) {
+        return {
+            seconds: 0,
+            minutes: 0,
+            hours: 0,
+            days: 0
+        };
+    }
+
     return {
         seconds: (start.getTime() - now.getTime()) / 1000,
         minutes: (start.getTime() - now.getTime()) / 1000 / 60,
